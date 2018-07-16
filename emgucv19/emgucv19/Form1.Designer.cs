f14cc3ce175fed81068ca8f54e6ec5099b61657b
078bilal <078bilal42@gmail.com>
2018-07-16 13:40:52 +0300

    emgucvline
    line

--- emgucv19/emgucv19/Form1.Designer.cs
+++ emgucv19/emgucv19/Form1.Designer.cs
@@ -32,20 +32,21 @@
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -82,6 +83,7 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
             this.splitContainer1.Panel1.Controls.Add(this.label1);
             this.splitContainer1.Panel1.Controls.Add(this.numericUpDown1);
             this.splitContainer1.Panel1.Controls.Add(this.button2);
@@ -95,44 +97,14 @@
             this.splitContainer1.SplitterDistance = 61;
             this.splitContainer1.TabIndex = 1;
             // 
-            // panel1
+            // label1
             // 
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(670, 388);
-            this.panel1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(163, 18);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Play";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(298, 19);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Pause";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(670, 388);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
             // 
             // numericUpDown1
             // 
@@ -151,14 +123,53 @@
             0,
             0});
             // 
-            // label1
+            // button2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(77, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.button2.Location = new System.Drawing.Point(298, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(670, 388);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(670, 388);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(607, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
@@ -177,9 +188,10 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,6 +209,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- emgucv19/emgucv19/Form1.cs
+++ emgucv19/emgucv19/Form1.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
 
 namespace emgucv19
 {
@@ -23,7 +26,24 @@ namespace emgucv19
         {
             InitializeComponent();
         }
-
+        public Point anchor { get; private set; }
+        public MCvScalar borderValue { get; private set; }
+        public BorderType borderType { get; private set; }
+        public IOutputArray hierarchy { get; private set; }
+        public object parent { get; private set; }
+        OpenFileDialog op = new OpenFileDialog();
+        UMat gimage = new UMat();
+        UMat bimage = new UMat();
+        UMat timage = new UMat();
+        UMat etimage = new UMat();
+        UMat dtimage = new UMat();
+        double egim = 1;
+        float[,] centerP = new float[3, 3];
+        UMat img = new UMat();
+        VectorOfVectorOfPoint cnst = new VectorOfVectorOfPoint();
+       // Image<Bgr, Byte> img;
+        double th = 200.0;
+        double thmax = 250.0;
         private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
         {
 
@@ -37,7 +57,7 @@ namespace emgucv19
                 capture = new VideoCapture(ofd.FileName);
                 Mat m = new Mat();
                 capture.Read(m);
-                pictureBox1.Image = m.Bitmap;
+               // pictureBox1.Image = m.Bitmap;
                 TotalFrame = capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameCount);
                 Fps = capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps);
             }
@@ -56,11 +76,50 @@ namespace emgucv19
         private async void ReadAllFrames()
         {
             Mat m = new Mat();
-            while (isReadingFrame==true && FrameNo<TotalFrame)
+            while (isReadingFrame==true && FrameNo<TotalFrame-1)
             {
                 FrameNo += Convert.ToInt16(numericUpDown1.Value);
                 capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.PosFrames, FrameNo);
                 capture.Read(m);
+                CvInvoke.CvtColor(m, gimage, ColorConversion.Bgr2Gray);
+                CvInvoke.GaussianBlur(gimage, bimage, new Size(7,7), 3, 0);
+                CvInvoke.Threshold(bimage, timage, th, thmax, ThresholdType.Binary);
+                CvInvoke.Erode(timage, etimage, null, anchor, 2, borderType, borderValue);
+                CvInvoke.Dilate(etimage, dtimage, null, anchor, 4, borderType, borderValue);
+                CvInvoke.FindContours(etimage, cnst, hierarchy, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+                Dictionary<int, double> dict = new Dictionary<int, double>();
+                for (int i = 0; i < cnst.Size; i++)
+                {
+                    double area = CvInvoke.ContourArea(cnst[i]);
+                    Rectangle rect = CvInvoke.BoundingRectangle(cnst[i]);
+                    CircleF crc = CvInvoke.MinEnclosingCircle(cnst[i]);
+                    dict.Add(i, area);
+                }
+                 
+                foreach (var item in dict)
+                {
+                    int key = int.Parse(item.Key.ToString());
+                    Rectangle rectt = CvInvoke.BoundingRectangle(cnst[key]);
+                    CvInvoke.Rectangle(m, rectt, new MCvScalar(0, 0, 255), 2);
+                    CvInvoke.PutText(m, key.ToString(), new Point(rectt.X, rectt.Y),0 , 0.45, new MCvScalar(255, 255, 255), 2);
+                    /*******AÇI BULDURUP DOĞRULARI ÇİZDİRMEEEEEEEEEEEEEE*******/
+                    centerP[key, 0] = rectt.X + (rectt.Width / 2);
+                    centerP[key, 1] = rectt.Y + (rectt.Height / 2);
+                    //CircleF ccf = CvInvoke.MinEnclosingCircle(cnst[key]);
+                    //Point cnter = new Point((int)ccf.Center.X,(int)ccf.Center.Y);
+                    //CvInvoke.Circle(img, cnter, (int)ccf.Radius, new MCvScalar(0, 0, 255),3);
+                    if (key > 0)
+                    {
+                        float y1, x1;
+                        y1 = centerP[key, 1] - centerP[key - 1, 1];
+                        x1 = centerP[key, 0] - centerP[key - 1, 0];
+                        CvInvoke.Line(m, new Point((int)centerP[key, 0], (int)centerP[key, 1]), new Point((int)centerP[key - 1, 0], (int)centerP[key - 1, 1]), new MCvScalar(0, 255, 0));
+                        centerP[key, 2] = y1 / x1;
+                        egim = centerP[key-1, 2] - centerP[key, 2] / ((centerP[key, 2] * centerP[key - 1, 2]) + 1);
+                       
+                    }
+                    Console.WriteLine(Math.Atan(egim) * (180 / Math.PI));
+                }
                 pictureBox1.Image = m.Bitmap;
                 await Task.Delay(1000 /Convert.ToInt16( Fps));
                 label1.Text = FrameNo.ToString() + "/" + TotalFrame.ToString();
@@ -72,5 +131,9 @@ namespace emgucv19
         {
             isReadingFrame = false;
         }
+
+        public FontFace fontFace { get; set; }
+
+        public double fontScale { get; set; }
     }
 }
